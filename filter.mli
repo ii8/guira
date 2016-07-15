8eee8e1a73d62614449eb45ea2b5fe8d3e2607da
Murray <murray.calavera@gmail.com>
2016-07-15 18:12:47 +0100

    clean gh-pages branch

--- filter.mli
+++
@@ -1,4 +0,0 @@
-
-val filter : Syntax.selector -> Time.t -> Time.t -> Time.interval -> bool
-
-val tests : (string * (unit -> bool)) list
--- sexp.mli
+++
@@ -1,8 +0,0 @@
-type sexp = Atom of string | List of sexp list
-
-val prerr_sexp : sexp -> unit
-
-val parse_stdin : unit -> sexp
-val parse_string : string -> sexp
-
-val tests : (string * (unit -> bool)) list
