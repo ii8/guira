b8265ec1f244f7be4ae5e1a5e21cac273203611d
Murray <murray.calavera@gmail.com>
2016-08-31 12:13:18 +0100

    web workers

---
+++ worker.js
@@ -0,0 +1,20 @@
+importScripts('guira.js')
+
+onmessage = function(e) {
+  var start = new Date();
+  var current = new Date();
+  var i = 100;
+
+  while (i) {
+    var b = false;
+
+    try {
+      b = guira(start.toISOString(), "day", e.data, current.toISOString());
+    } catch (ex) {}
+
+    if (b)
+      postMessage(i-- == 1 ? "..." : current.toDateString());
+
+    current.setDate(current.getDate() + 1);
+  }
+}
